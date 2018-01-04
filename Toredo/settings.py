8428d6113b37dbb27c935d98917819d2f61ea628
Simon <simlardinois109@gmail.com>
2018-01-04 20:29:01 +0100

    commit

--- Toredo/settings.py
+++ Toredo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cb!l6$x=%umznq2@i^n8o0u-y8!19enmt-fssw_hik=y^u#0u#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.24']
 
 
 # Application definition
